dbaa295c734e3b1fcf35ff8a74ee8334398f0d06
Tushar <t.singhal2003@gmail.com>
2025-07-10 05:28:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const io = new Server(server, {
 });
 
 app.use(cors({
-  origin: "http://localhost:8080",
+  origin: "https://jobnest-project-fork-pbpi697pb-tsinghalbe22s-projects.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
